c921abb061258b31ad57deac795a9f9da3ff6900
csawai <csawai@gmail.com>
2022-02-01 11:54:07 +0530

    added the basic setup for nums

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import fmt
+
+func main () {
+var number1, number2 float64
+var operator string 
+
+fmt.Print("enter the first number")
+fmt.Scanln(&number1)
+
+fmt.Print("enter the second number")
+fmt.Scanln(&number2)
+
+fmt.Print("Enter the operator")
+fmt.Scanln(&operator)
+
+
+}
